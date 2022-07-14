356c963f287473a0778d9145b92c67b04e9983c6
LucasReinhard <lucas-reinhard@gmx.net>
2022-07-14 15:18:40 +0200

    added AND gate spawner (experimental)

---
+++ Assets/Scripts/Clonable.cs
@@ -0,0 +1,19 @@
+﻿using HTC.UnityPlugin.ColliderEvent;
+using UnityEngine;
+
+namespace TILab
+{
+    public class Clonable : MonoBehaviour, IColliderEventDragStartHandler
+    {
+        public GameObject spawnableItem;
+        
+        public void OnColliderEventDragStart(ColliderButtonEventData eventData)
+        {
+            Debug.Log(eventData);
+            if (eventData.button == ColliderButtonEventData.InputButton.GripOrHandTrigger)
+            {
+                Instantiate(spawnableItem, this.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
