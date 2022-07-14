0259caf433d3688f5bc6be8eba293a600a1d2221
Julian Paulus <julianpaulus704@gmail.com>
2022-07-14 13:30:01 +0200

    give deletable a target to remove, if parent needs to be removed too

--- Assets/Scripts/Deletable.cs
+++ Assets/Scripts/Deletable.cs
@@ -6,11 +6,20 @@ namespace TILab
     public class Deletable : MonoBehaviour, IColliderEventDragStartHandler
     {
         public bool deletable = true;
+        public GameObject toDelete;
         public void OnColliderEventDragStart(ColliderButtonEventData eventData)
         {
+            Debug.Log(eventData);
             if (deletable && eventData.button == Config.RemoveButton)
             {
-                Destroy(gameObject);   
+                if (toDelete != null)
+                {
+                    Destroy(toDelete);
+                }
+                else
+                {
+                    Destroy(gameObject);   
+                }
             }
         }
     }
