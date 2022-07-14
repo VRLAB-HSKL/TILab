6ad94f3c2a9f189995a5c6269f9bf7677b2fde7a
LucasReinhard <lucas-reinhard@gmx.net>
2022-07-14 12:56:03 +0200

    make gates removable create script "deletable" make Connectedcable use deletable script

---
+++ Assets/Scripts/Deletable.cs
@@ -0,0 +1,17 @@
+﻿using HTC.UnityPlugin.ColliderEvent;
+using UnityEngine;
+
+namespace TILab
+{
+    public class Deletable : MonoBehaviour, IColliderEventDragStartHandler
+    {
+        public bool deletable = false;
+        public void OnColliderEventDragStart(ColliderButtonEventData eventData)
+        {
+            if (deletable && eventData.button == Config.RemoveButton)
+            {
+                Destroy(gameObject);   
+            }
+        }
+    }
+}
--- Assets/Scripts/Gates/Gate.cs
+++ Assets/Scripts/Gates/Gate.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using HTC.UnityPlugin.ColliderEvent;
 using UnityEngine;
 
 namespace TILab
