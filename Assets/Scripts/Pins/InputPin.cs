0cfcdc30603684bce047b0ef48ca7a322a4b3030
Julian Paulus <julianpaulus704@gmail.com>
2022-07-19 20:14:07 +0200

    fix pins staying on when a powered cable gets disconnected

--- Assets/Scripts/Pins/InputPin.cs
+++ Assets/Scripts/Pins/InputPin.cs
@@ -5,5 +5,14 @@ namespace TILab
 {
     public class InputPin : Pin
     {
+        public override void Disconnect(ConnectedCable cable)
+        {
+            base.Disconnect(cable);
+            Value = false;
+            if (_cables.Count == 0)
+            {
+                OnCircuitUpdate();
+            }
+        }
     }
 }
