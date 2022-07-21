68b88c8a53189c16f539e31f8e1602aa7a418d00
Maurice Didion <Saarli@users.noreply.github.com>
2022-07-21 18:47:38 +0200

    Addierer an 3 Leveloutput angepasst

--- Assets/Scripts/Tester/AddierLevel.cs
+++ Assets/Scripts/Tester/AddierLevel.cs
@@ -7,22 +7,22 @@ namespace TILab.Tester
             base.Start();
             Sequence = new SequenceItem[]
             {
-                new SequenceItem("0000", "0000"),
-                new SequenceItem("0001", "0001"),
-                new SequenceItem("0010", "0010"),
-                new SequenceItem("0011", "0011"),
-                new SequenceItem("0100", "0001"),
-                new SequenceItem("0101", "0010"),
-                new SequenceItem("0110", "0011"),
-                new SequenceItem("0111", "0100"),
-                new SequenceItem("1000", "0010"),
-                new SequenceItem("1001", "0011"),
-                new SequenceItem("1010", "0100"),
-                new SequenceItem("1011", "0101"),
-                new SequenceItem("1100", "0011"),
-                new SequenceItem("1101", "00100"),
-                new SequenceItem("1110", "00101"),
-                new SequenceItem("1111", "0110"),
+                new SequenceItem("0000", "000"),
+                new SequenceItem("0001", "001"),
+                new SequenceItem("0010", "010"),
+                new SequenceItem("0011", "011"),
+                new SequenceItem("0100", "001"),
+                new SequenceItem("0101", "010"),
+                new SequenceItem("0110", "011"),
+                new SequenceItem("0111", "100"),
+                new SequenceItem("1000", "010"),
+                new SequenceItem("1001", "011"),
+                new SequenceItem("1010", "100"),
+                new SequenceItem("1011", "101"),
+                new SequenceItem("1100", "011"),
+                new SequenceItem("1101", "100"),
+                new SequenceItem("1110", "101"),
+                new SequenceItem("1111", "110"),
             };
         }
     }
