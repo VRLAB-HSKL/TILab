fdd3017fdf34f23f87bf4f5a6e0f63a6b3727f83
LucasReinhard <lucas-reinhard@gmx.net>
2022-07-22 10:13:53 +0200

    prettier Latch Level fixed Latch State Table

--- Assets/Scripts/Tester/LatchLevel.cs
+++ Assets/Scripts/Tester/LatchLevel.cs
@@ -8,9 +8,9 @@ namespace TILab.Tester
             // TODO: @Saarli fix this
             Sequence = new SequenceItem[]
             {
-                new SequenceItem("10", "10"),
+                new SequenceItem("01", "10"),
                 new SequenceItem("00", "10"),
-                new SequenceItem("01", "01"),
+                new SequenceItem("10", "01"),
                 new SequenceItem("00", "01"),
             };
         }
