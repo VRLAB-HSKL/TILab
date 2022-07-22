ba0705e46792aa60dd98748871c9aed10b807167
LucasReinhard <lucas-reinhard@gmx.net>
2022-07-22 09:58:08 +0200

    remove chaotic adder level add new half-adder level

--- Assets/Scripts/Tester/AddierLevel.cs
+++ Assets/Scripts/Tester/AddierLevel.cs
@@ -7,22 +7,10 @@ namespace TILab.Tester
             base.Start();
             Sequence = new SequenceItem[]
             {
-                new SequenceItem("0000", "000"),
-                new SequenceItem("0001", "001"),
-                new SequenceItem("0010", "010"),
-                new SequenceItem("0011", "011"),
-                new SequenceItem("0100", "001"),
-                new SequenceItem("0101", "010"),
-                new SequenceItem("0110", "011"),
-                new SequenceItem("0111", "100"),
-                new SequenceItem("1000", "010"),
-                new SequenceItem("1001", "011"),
-                new SequenceItem("1010", "100"),
-                new SequenceItem("1011", "101"),
-                new SequenceItem("1100", "011"),
-                new SequenceItem("1101", "100"),
-                new SequenceItem("1110", "101"),
-                new SequenceItem("1111", "110"),
+                new SequenceItem("00", "00"),
+                new SequenceItem("10", "01"),
+                new SequenceItem("01", "01"),
+                new SequenceItem("11", "10"),
             };
         }
     }
